e46a25aded0ed93c04b634a4c533c31ea7a1a702
muhammad javed <javedmohd355@gmail.com>
2025-03-15 20:40:34 -0500

    darkmode

---
+++ country.js
@@ -0,0 +1,9 @@
+const h1=document.querySelector('h1');
+
+
+const countryName= new URLSearchParams(window.location.search).get('name');
+fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`).then((res)=>res.json().then((data)=>
+console.log(data[0])))
+h1.innerHTML=countryName
+
+// (`https://restcountries.com/v3.1/name${countryName}?fullText=true`)
