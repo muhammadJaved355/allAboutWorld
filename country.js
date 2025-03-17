5f15a5b94faeaed1dc788ce60e98bc02daf1b7f3
muhammad javed <javedmohd355@gmail.com>
2025-03-17 04:58:25 -0500

    country Details

--- country.js
+++ country.js
@@ -1,9 +1,28 @@
-const h1=document.querySelector('h1');
 
+const coundryDetails=document.querySelector('.coundryDetails');
 
 const countryName= new URLSearchParams(window.location.search).get('name');
-fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`).then((res)=>res.json().then((data)=>
-console.log(data[0])))
-h1.innerHTML=countryName
+fetch(`https://restcountries.com/v3.1/name/${countryName}`).then((res)=>res.json().then((data)=>{
+console.log(data)
+  data.forEach(element => {
+  console.log(element)
+ coundryDetails.innerHTML=`
+ 
+  <img src="${element.flags.svg}" alt="flag">
+      <div class="detailTextContainer">
+        <h2>${element.name.common}</h2>
+      <div class="detail">
+        <p><b>Native Name : </b>${element.name.nativeName.official?element.name.nativeName.official:"N/A"}</p>
+        <p><b>Population: </b>${element.population}</p>
+        <p><b>Region: </b>${element.region}</p>
+        <p><b>Sub Region: </b>${element.subregion}</p>
+        <p><b>Capital: </b>${element.capital}</p>
+        <p><b>Top Level Domain: </b>${element.domain?element.domain:"N/A"}</p>
+        <p><b>Currencies : </b>${element.currencies.EUR}</p>
+        <p><b>Languages : </b>${element.languages.fra}</p>
+ `
+  });
 
-// (`https://restcountries.com/v3.1/name${countryName}?fullText=true`)
+}
+))
+
--- script.js
+++ script.js
@@ -4,27 +4,27 @@ const countriesContainer = document.querySelector('.countryContainer')
   fetch(`https://restcountries.com/v3.1/all`)
   .then((res)=>res.json())
   .then((data)=>{
-    console.log(data)
+    
+    
     data.forEach(country => {
-      
       const countryCard = document.createElement('a');
       countryCard.classList.add('country_card');
       countryCard.href=`/country.html?name=${country.name.common}`
       
       countryCard.innerHTML=`
-  <img src="${country.flags.svg}" alt="flag">
+  <img src="${country.flags.svg}" alt="${country.name.common}">
        <div class="cardText"> 
         <h3 class="cardTitle">${country.name.common}</h3>
         <p><b>Population: </b>${country.population.toLocaleString('en-IN')}</p>
         <p><b>Region: </b>${country.region}</p>
-        <p><b>Capital: </b>${country.capital?.[0]}</p>
+        <p><b>Capital: </b>${country.capital}</p>
         </div>
 `
 countriesContainer.append(countryCard);
-    });
+});
+
+});
   
-  });
-  
-  btnToggle.addEventListener('click',()=>{
-    document.body.classList.toggle('dark-mode')
-  })
+  // btnToggle.addEventListener('click',()=>{
+  //   document.body.classList.toggle('dark-mode')
+  // })
